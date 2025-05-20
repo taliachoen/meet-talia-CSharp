c0c48c22a931bf7771e48f5bcbe19c5712202805
taliachoen <147976038+taliachoen@users.noreply.github.com>
2025-05-20 20:41:29 +0300

    Fix CORS middleware order

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-// הוסף CORS לפני קריאה ל Build()
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -14,7 +14,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// קודם הפעלת הפנייה ל-HTTPS
+app.UseHttpsRedirection();
+
+// אחרי זה הפעלת CORS
 app.UseCors("AllowAll");
+
+// אם יש צורך ב-authorization אז אחרי CORS
+// app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,15 +29,13 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/weatherforecast", () =>
 {
+    var summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
@@ -51,3 +56,60 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+
+
+
+// var builder = WebApplication.CreateBuilder(args);
+// builder.Services.AddControllers();
+// // הוסף CORS לפני קריאה ל Build()
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowAll",
+//         policy =>
+//         {
+//             policy.AllowAnyOrigin()
+//                   .AllowAnyHeader()
+//                   .AllowAnyMethod();
+//         });
+// });
+
+// var app = builder.Build();
+
+// app.UseCors("AllowAll");
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
+
+// app.UseHttpsRedirection();
+
+// var summaries = new[]
+// {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
+
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast = Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast");
+
+// app.MapControllers();
+
+// app.Run();
+
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
