319d2048de31e9b09e348826894edfa5c2194946
taliachoen <147976038+taliachoen@users.noreply.github.com>
2025-05-21 10:37:50 +0300

    check

--- server/Controllers/AllControllers.cs
+++ server/Controllers/AllControllers.cs
@@ -65,12 +65,6 @@ namespace MeetTaliaAPI.Controllers
             _config = config;
         }
 
-         private readonly ILogger<AiController> _logger;
-
-    public AiController(ILogger<AiController> logger)
-    {
-        _logger = logger;
-    }
 
         [HttpPost("analyze-fit")]
         public async Task<IActionResult> AnalyzeFit([FromBody] JobRequest request)
